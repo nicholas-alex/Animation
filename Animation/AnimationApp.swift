1eba7bb3a0d8f1b409468d3efbc504832c119ba2
nico <nixcp.cpm>
2021-09-23 16:47:26 +0200

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Nicholas on 23/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
