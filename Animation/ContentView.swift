ac2bd378bf75b20addcdacc17f09d663887b8046
nico <nixcp.cpm>
2021-09-23 16:47:26 +0200

    Initial Commit

---
+++ Animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animation
+//
+//  Created by Nicholas on 23/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
